7f15ab409308476779b9dbcc7e644fa7fb4b3c86
AdrienClairembault <aclairembault@teclib.com>
2021-07-16 16:59:16 +0200

    Fix import order (lint)

--- inc/commonitilobject.class.php
+++ inc/commonitilobject.class.php
@@ -34,9 +34,9 @@ if (!defined('GLPI_ROOT')) {
    die("Sorry. You can't access this file directly");
 }
 
-use Glpi\Toolbox\RichText;
 use Glpi\ContentTemplates\Parameters\CommonITILObjectParameters;
 use Glpi\ContentTemplates\TemplateManager;
+use Glpi\Toolbox\RichText;
 
 /**
  * CommonITILObject Class
